b5248c650c5b775c6ae9a771b36ad636777a379b
vitor <eletrotecvitor@gmail.com>
2024-02-12 21:29:44 -0300

    trabalhando a responsividade e fazendo a inserção dos produtos dinamicamente

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,28 @@
-console.log('jordan shoes')
+const getProducts = async () => {
+    const response = await fetch('Js/products.json')
+    const data = await response.json()
+    return data
+}
+
+const generateCard = async () => {
+    const products = await getProducts()
+    products.map(product => {
+        let card = document.createElement('div')
+        card.classList.add('card__produto')
+        card.innerHTML = `
+<figure>
+        <img src="Imagens/${product.image}" alt="${product.product_name}" />
+    </figure>
+    <div class="card__produto_detalhes">
+        <h4>${product.product_name}</h4>
+        <h5>${product.product_model}</h5>
+    </div>
+    <h6>R$ ${product.price}</h6>
+        `
+    
+    const listaProdutos = document.querySelector('.lista__produtos')
+    listaProdutos.appendChild(card)
+    })
+}
+
+generateCard()
